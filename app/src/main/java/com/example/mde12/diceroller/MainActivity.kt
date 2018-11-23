5bdf2bf8fcba776d72fd3a53c0c4a3abeccd9253
Matthew Dewhirst <matthew.dewhirst@skybettingandgaming.com>
2018-11-23 14:00:48 +0000

    passing total dice and die value to dice roller fun. also made text view scrollable

--- app/src/main/java/com/example/mde12/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/mde12/diceroller/MainActivity.kt
@@ -2,9 +2,10 @@ package com.example.mde12.diceroller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.text.method.ScrollingMovementMethod
 import android.view.View
 import android.widget.Button
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.outputView
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
@@ -12,28 +13,35 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        outputView.setMovementMethod(ScrollingMovementMethod())
     }
+
+    private var totalDice: String = "0"
 
     fun onClickIntBtn(v: View) {
         val b = v as Button
         val buttonText : String = b.text.toString()
         outputView.append(buttonText)
+        totalDice += buttonText
     }
 
     fun onClickDieBtn(v: View) {
         val b = v as Button
         val buttonText : String = b.text.toString()
         outputView.append(buttonText + "\n")
-        // trigger die rolling logic
-        rollDice(10, 20)
+
+        rollDice(buttonText.removePrefix("d").toInt())
     }
 
-    private fun rollDice(totalDice: Int, dieValue: Int) {
+    private fun rollDice(dieValue: Int) {
         outputView.append("Result >>> ")
-        val totalDiceMin : Int = if (totalDice > 0) totalDice else 1
+        val totalDiceMin : Int = if (totalDice.toInt() > 0) totalDice.toInt() else 1
         for (i in 1..totalDiceMin) {
-            outputView.append(Random.nextInt(0, dieValue + 1).toString() + " ")
+            outputView.append(Random.nextInt(1, dieValue + 1).toString() + " ")
         }
-        outputView.append("\n")
+        outputView.append("\n\n")
+
+        totalDice = "0"
     }
 }
