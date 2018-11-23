e44beaf657db9b7638b34e6b35bb1c18e47eb533
Matthew Dewhirst <matthew.dewhirst@skybettingandgaming.com>
2018-11-23 13:21:19 +0000

    added dice rolling logic

--- app/src/main/java/com/example/mde12/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/mde12/diceroller/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,12 +17,23 @@ class MainActivity : AppCompatActivity() {
     fun onClickIntBtn(v: View) {
         val b = v as Button
         val buttonText : String = b.text.toString()
-        outputView.append( buttonText )
+        outputView.append(buttonText)
     }
 
     fun onClickDieBtn(v: View) {
         val b = v as Button
         val buttonText : String = b.text.toString()
-        outputView.append( buttonText + "\n" )
+        outputView.append(buttonText + "\n")
+        // trigger die rolling logic
+        rollDice(10, 20)
+    }
+
+    private fun rollDice(totalDice: Int, dieValue: Int) {
+        outputView.append("Result >>> ")
+        val totalDiceMin : Int = if (totalDice > 0) totalDice else 1
+        for (i in 1..totalDiceMin) {
+            outputView.append(Random.nextInt(0, dieValue + 1).toString() + " ")
+        }
+        outputView.append("\n")
     }
 }
