d957114519412def13b7a572e1925d671a933af3
Matthew Dewhirst <matthew.dewhirst@skybettingandgaming.com>
2018-11-23 13:02:36 +0000

    added text view and button on click functions

--- app/src/main/java/com/example/mde12/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/mde12/diceroller/MainActivity.kt
@@ -2,11 +2,26 @@ package com.example.mde12.diceroller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun onClickIntBtn(v: View) {
+        val b = v as Button
+        val buttonText : String = b.text.toString()
+        outputView.append( buttonText )
+    }
+
+    fun onClickDieBtn(v: View) {
+        val b = v as Button
+        val buttonText : String = b.text.toString()
+        outputView.append( buttonText + "\n" )
     }
 }
