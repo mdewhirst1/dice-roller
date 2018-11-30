5d8a86bd7a60a4c8c99e4576d27a7b61af3c2dc9
Matthew Dewhirst <matthew.dewhirst@skybettingandgaming.com>
2018-11-30 15:56:22 +0000

    added scrollable tet that starts at top of screen and auto scrolls down ugly but works

--- app/src/main/java/com/example/mde12/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/mde12/diceroller/MainActivity.kt
@@ -2,22 +2,25 @@ package com.example.mde12.diceroller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.text.method.ScrollingMovementMethod
 import android.view.View
 import android.widget.Button
+import android.widget.ScrollView
 import kotlinx.android.synthetic.main.activity_main.outputView
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
+    private var totalDice: String = "0"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        outputView.setMovementMethod(ScrollingMovementMethod())
     }
 
-    private var totalDice: String = "0"
+    private fun scrollDown() {
+        val scrollview : ScrollView = findViewById(R.id.outputScrollView)
+        scrollview.post { scrollview.fullScroll(ScrollView.FOCUS_DOWN) }
+    }
 
     fun onClickIntBtn(view: View) {
         val b = view as Button
@@ -29,29 +32,30 @@ class MainActivity : AppCompatActivity() {
     fun onClickDieBtn(view: View) {
         val b = view as Button
         val buttonText : String = b.text.toString()
-        outputView.append(buttonText + "\n")
+        outputView.append(buttonText)
 
         rollDice(buttonText.removePrefix("d").toInt())
+        scrollDown()
     }
 
     @Suppress("UNUSED_PARAMETER")
     fun onClickDelBtn(view: View) {
         if (totalDice.length > 1) {
             totalDice = totalDice.substring(0, totalDice.length - 1)
-            outputView.setText(outputView.text.substring(0, outputView.text.length - 1))
+            outputView.text = outputView.text.substring(0, outputView.text.length - 1)
         }
     }
 
     @Suppress("UNUSED_PARAMETER")
     fun onClickClrBtn(view: View) {
-        outputView.setText(outputView.text.substring(0, outputView.text.length - (totalDice.length - 1)))
+        outputView.text = outputView.text.substring(0, outputView.text.length - (totalDice.length - 1))
         totalDice = "0"
     }
 
     private fun rollDice(dieValue: Int) {
-        outputView.append("Result >>> ")
-        val totalDiceMin : Int = if (totalDice.toInt() > 0) totalDice.toInt() else 1
-        for (i in 1..totalDiceMin) {
+        outputView.append("\n\nResult >>> ")
+        val totalDiceInt : Int = if (totalDice.toInt() > 0) totalDice.toInt() else 1
+        for (i in 1..totalDiceInt) {
             outputView.append(Random.nextInt(1, dieValue + 1).toString() + " ")
         }
         outputView.append("\n\n")
